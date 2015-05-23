8589c9efdd2c70b07e0e24321e7f07c285481746
Eray Aydın <eray@erayaydin.me>
2015-05-23 19:10:43 +0300

    Index.html generator prototype

--- app/controllers/GenerateController.php
+++ app/controllers/GenerateController.php
@@ -21,8 +21,40 @@ class GenerateController extends BaseController {
         if($validator->fails())
             return \Redirect::route("generate")->withInput()->withErrors($validator);
 
-        // Generate index.html
+        $favicon     = Input::has("favicon") && Input::get("favicon") != "" ? true : false;
+        $opengraph   = Input::has("opengraph") && Input::get("opengraph") != "" ? true : false;
+        $twittercard = Input::has("twittercard") && Input::get("twittercard") != "" ? true : false;
 
+        $jquery = Input::has("jquery") && Input::get("jquery") != "" ? true : false;
+        $jqueryui = Input::has("jqueryui") && Input::get("jqueryui") != "" ? true : false;
+        $normalize = Input::has("normalize") && Input::get("normalize") != "" ? true : false;
+        $meyerreset = Input::has("meyerreset") && Input::get("meyerreset") != "" ? true : false;
+        $fontawesome = Input::has("fontawesome") && Input::get("fontawesome") != "" ? true : false;
+        $animate = Input::has("animate") && Input::get("animate") != "" ? true : false;
+        $bootstrap = Input::has("bootstrap") && Input::get("bootstrap") != "" ? true : false;
+        $prettyphoto= Input::has("prettyphoto") && Input::get("prettyphoto") != "" ? true : false;
+
+        $indexContent = \View::make("source")->with(array(
+            "project_name"  => Input::get("project_name"),
+            "asset_name"    => Input::get("asset_name"),
+            "image_name"    => Input::get("image_name"),
+            "css_name"      => Input::get("css_name"),
+            "js_name"       => Input::get("js_name"),
+            "plugin_name"   => Input::get("plugin_name"),
+            "favicon"       => $favicon,
+            "opengraph"     => $opengraph,
+            "twittercard"   => $twittercard,
+            "jquery"        => $jquery,
+            "jqueryui"      => $jqueryui,
+            "normalize"     => $normalize,
+            "meyerreset"    => $meyerreset,
+            "fontawesome"   => $fontawesome,
+            "animate"       => $animate,
+            "bootstrap"     => $bootstrap,
+            "prettyphoto"   => $prettyphoto
+        ));
+
+        return $indexContent;
         // Generate project.zip
 
         // Download project.zip
