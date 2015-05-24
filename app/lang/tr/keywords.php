ff3bc13f02baeb129a4d6488ee8457c7869f9734
Eray Aydın <eray@erayaydin.me>
2015-05-24 18:12:56 +0300

    Turkish language support added

---
+++ app/lang/tr/keywords.php
@@ -0,0 +1,19 @@
+<?php
+
+return array(
+    "togglenavigation" => "Menüyü aç",
+    "generate"         => "Oluştur",
+    "current"          => "buradasınız",
+    "beta"             => "BETA",
+    "general"          => "Genel",
+    "projectname"      => "Proje İsmi",
+    "zipfilename"      => "Zip Dosya Adı",
+    "assetdirname"     => "Asset Klasör Adı",
+    "default"          => "Varsayılan: :name",
+    "imagedirname"     => "Resimler Klasör Adı",
+    "cssdirname"       => "CSS Klasör Adı",
+    "jsdirname"        => "JS Klasör Adı",
+    "plugindirname"    => "Eklentiler Klasör Adı",
+    "assets"           => "Assets",
+    "misc"             => "Diğer",
+);
