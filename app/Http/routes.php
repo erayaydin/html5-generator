a5038302b401f39224eaf67ba6cd2f8e8f4644cd
Eray Aydın <eray@erayaydin.me>
2015-09-02 15:48:46 +0300

    Added basic routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', array("as" => "homepage", "uses" => "HomeController@index"));
+Route::get("generate", array("as" => "generate", "uses" => "GenerateController@create"));
+Route::post("generate/store", array("as" => "generate.store", "uses" => "GenerateController@store"));
