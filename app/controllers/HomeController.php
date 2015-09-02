c85c1c8760d0db029f4bb9ba421a2f302bb7f35b
Eray Aydın <eray@erayaydin.me>
2015-09-02 15:41:07 +0300

    Deleted everything for laravel 5.1

--- app/controllers/HomeController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class HomeController extends BaseController {
-
-	public function index() {
-        return \Redirect::route("generate");
-    }
-
-}
--- app/routes.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-Route::get('/', array("as" => "homepage", "uses" => "HomeController@index"));
-Route::get("generate", array("as" => "generate", "uses" => "GenerateController@create"));
-Route::post("generate/store", array("as" => "generate.store", "uses" => "GenerateController@store"));
